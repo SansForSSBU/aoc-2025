328d4f7bf7a985c79dee060f7375051c877210b3
SansForSSBU <taylorjoseph3333@gmail.com>
2026-01-03 17:45:30 +0000

    WIP

---
+++ puzzle3/solution.py
@@ -0,0 +1,24 @@
+with open("puzzle3/input.txt", "r") as f:
+    input_text = f.read()
+lines = [l for l in input_text.split("\n") if len(l)>0]
+
+def get_max_joltage_pt2(line):
+    pass
+    return 0
+
+def get_max_joltage_pt1(line):
+    nums = [int(x) for x in list(line)]
+    first_num = max(nums[:-1])
+    second_num = max(nums[nums.index(first_num)+1:])
+    return first_num*10+second_num
+
+def solve_pt1(lines):
+    ans = sum([get_max_joltage_pt1(l) for l in lines])
+    print("Part 1 answer:", ans)
+
+def solve_pt2(lines):
+    ans = sum([get_max_joltage_pt2(l) for l in lines])
+    print("Part 2 answer:", ans)
+
+solve_pt1(lines)
+solve_pt2(lines)
