6643abca971f176dcbf31590058543b7a11be36a
SansForSSBU <taylorjoseph3333@gmail.com>
2026-01-03 18:03:30 +0000

    WIP

--- puzzle3/solution.py
+++ puzzle3/solution.py
@@ -2,10 +2,10 @@ with open("puzzle3/input.txt", "r") as f:
     input_text = f.read()
 lines = [l for l in input_text.split("\n") if len(l)>0]
 
-def get_max_joltage_pt2(line):
+def get_max_joltage(line, bulbs):
     nums = [int(x) for x in list(line)]
     ret = ""
-    for digit in range(12, 0, -1):
+    for digit in range(bulbs, 0, -1):
         excluded_digits = digit-1
         if excluded_digits > 0:
             choices = nums[:-excluded_digits]
@@ -17,19 +17,5 @@ def get_max_joltage_pt2(line):
         ret = ret + str(largest)
     return int(ret)
 
-def get_max_joltage_pt1(line):
-    nums = [int(x) for x in list(line)]
-    first_num = max(nums[:-1])
-    second_num = max(nums[nums.index(first_num)+1:])
-    return first_num*10+second_num
-
-def solve_pt1(lines):
-    ans = sum([get_max_joltage_pt1(l) for l in lines])
-    print("Part 1 answer:", ans)
-
-def solve_pt2(lines):
-    ans = sum([get_max_joltage_pt2(l) for l in lines])
-    print("Part 2 answer:", ans)
-
-solve_pt1(lines)
-solve_pt2(lines)
+print("Part 1 answer:", sum([get_max_joltage(l,2) for l in lines]))
+print("Part 2 answer:", sum([get_max_joltage(l,12) for l in lines]))
