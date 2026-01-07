78c257c37969be29659cf0d3175271b4fe913bf1
SansForSSBU <taylorjoseph3333@gmail.com>
2026-01-07 18:15:56 +0000

    Restructure inputs and solutions

---
+++ src/solutions/puzzle12/solution.py
@@ -0,0 +1,42 @@
+import numpy as np
+from math import prod
+with open("inputs/12.txt") as f:
+    input_text = f.read()
+
+sections = input_text.split("\n\n")
+present_text = sections[:-1]
+problems_text = sections[-1]
+
+presents = []
+for present in present_text:
+    present_idx, present_shape = present.split(":\n")
+    present_idx = int(present_idx)
+    present_shape_lines = present_shape.split("\n")
+    present_shape = np.array([[1 if chr == '#' else 0 for chr in line] for line in present_shape_lines])
+    presents.append(present_shape)
+
+problems = []
+for problem in [p for p in problems_text.split("\n") if len(p) > 0]:
+    spec, amounts = problem.split(": ")
+    dims = [int(x) for x in spec.split("x")]
+    amounts = [int(x) for x in amounts.split(" ")]
+    problems.append((dims, amounts))
+
+def solve_problem(problem):
+    dims, nums = problem
+    total_space = dims[0]*dims[1]
+    min_occupied = sum([v*sum(sum(presents[i])) for i,v in enumerate(nums)])
+    if min_occupied > total_space:
+        return False
+    
+    max_occupied = [prod(present.shape) for present in presents]
+    if total_space >= sum([num * max_occupied[i] for i, num in enumerate(nums)]):
+        return True
+    
+    raise Exception("Naive solution won't work.")
+
+n = 0
+for problem in problems:
+    if solve_problem(problem):
+        n += 1
+print("Part 1 solution:", n)
---
+++ src/solutions/puzzle2/solution.py
@@ -0,0 +1,38 @@
+import math
+
+with open("inputs/2.txt", "r") as f:
+    lines = f.read()
+
+ranges = [[int(num) for num in line.split("-")] for line in lines.split(",")]
+ranges = [range(r[0], r[1]+1) for r in ranges]
+
+def is_factor(factor, number):
+    return number % factor == 0
+
+def all_numbers_with_n_digits(n):
+    return list(range(1, 10**n))
+
+def make_invalids_with_n_digits(n, part1=False):
+    invalids = []
+    for totalLength in range(1,n+1):
+        for repeatedLength in [number for number in list(range(1,totalLength)) if is_factor(number,totalLength)]:
+            repeats = int(totalLength / repeatedLength)
+            if part1 and repeats != 2:
+                continue
+            invalids.extend([int(str(x)*repeats) for x in all_numbers_with_n_digits(repeatedLength)])
+    return list(set(invalids))
+
+def solve_part(ranges, invalids):
+    ans = 0
+    for r in ranges:
+        for invalid in invalids:
+            if invalid in r:
+                ans += invalid
+    return ans
+
+max_n_digits = math.ceil(math.log10(max([max(r) for r in ranges])))
+invalids_part1 = make_invalids_with_n_digits(max_n_digits, part1=True)
+invalids_part2 = make_invalids_with_n_digits(max_n_digits, part1=False)
+
+print("Part 1 ans: ", solve_part(ranges, invalids_part1))
+print("Part 1 ans: ", solve_part(ranges, invalids_part2))
