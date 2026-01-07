ba7e52c8b08f46335579a2451464d3ab6645959c
SansForSSBU <taylorjoseph3333@gmail.com>
2026-01-07 00:01:06 +0000

    WIP

--- puzzle11/solution.py
+++ puzzle11/solution.py
@@ -8,32 +8,30 @@ for line in lines:
     out_nodes = out_nodes.split(" ")
     connections[in_node] = out_nodes
 
+def get_upstream_nodes(graph, node):
+    return [k for k,v in graph.items() if node in v]
 
 def find_n_routes(graph, start, end):
-    # 599
-    # TODO: Optimize
-    unexplored_routes = [start]
-    num_routes = {start: 1}
-    routes = set()
-    idx = 0
-    while len(unexplored_routes) > 0:
-        node = unexplored_routes.pop(0)
-        if node == "out":
-            continue
-        outputs = graph[node]
-        for output in outputs:
-            num_routes[output] = num_routes.get(output,0) + num_routes[node]
-            unexplored_routes.append(output)
-        if node == end:
-            continue
-        num_routes[node] = 0
-    return num_routes[end]
+    kv = {end: 1}
+    routes = [(end, 0)]
+    while len(routes) > 0:
+        routes.sort(key=lambda x: x[1])
+        node, depth = routes.pop(0)
+        upstream_nodes = get_upstream_nodes(graph, node)
+        for up in upstream_nodes:
+            if not up in kv.keys():
+                routes.append((up, depth+1))
+            kv[up] = kv.get(up,0) + kv[node]
+    
+    return kv.get(start, 0)
+
 
 def solve_pt1(graph):
     return find_n_routes(graph, "you", "out")
     
 
 def solve_pt2(graph):
+    # 11350111374360 too low
     n1 = find_n_routes(graph, "svr", "fft")
     n2 = find_n_routes(graph, "fft", "dac")
     n3 = find_n_routes(graph, "dac", "out")
