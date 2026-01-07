b2b54abb5f9dbedef485a0f7c1b3ed6912fd19d1
SansForSSBU <taylorjoseph3333@gmail.com>
2026-01-07 19:01:53 +0000

    Refactor first test

--- tests/test_solutions.py
+++ tests/test_solutions.py
@@ -1,10 +1,9 @@
 import pytest
-from solutions.puzzle1.solution import main
+from solutions.puzzle1.solution import main as solution1
 
 def test_solution1():
     input_path = "tests/my_inputs/1.txt"
     with open(input_path, "r") as f:
         input_text = f.read()
-    pt1_ans, pt2_ans = main(input_text)
-    assert pt1_ans == 1052
-    assert pt2_ans == 6295
+    answers = solution1(input_text)
+    assert answers == (1052, 6295)
