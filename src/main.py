939a0a7f002947ed28f2d337c910e0c403bf4199
SansForSSBU <taylorjoseph3333@gmail.com>
2026-01-07 18:57:40 +0000

    Make main print out the answers nicely

--- src/main.py
+++ src/main.py
@@ -5,7 +5,9 @@ def my_main():
     puzzle_num = sys.argv[1]
     with open(f"inputs/{puzzle_num}.txt", "r") as f:
         input_file = f.read()
-    print(main(input_file))
+    answers = main(input_file)
+    for idx, answer in enumerate(answers):
+        print(f"Part {idx+1} answer: {answer}")
 
 if __name__ == "__main__":
     my_main()
