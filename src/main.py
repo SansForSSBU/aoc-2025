a8474cb4ded75e45822080d44d88041b3433d318
SansForSSBU <taylorjoseph3333@gmail.com>
2026-01-07 18:47:35 +0000

    Add debug configuration. Make main.py take puzzle_num as a command line argument

--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
+import sys
+
 def my_main():
     from solutions.puzzle1.solution import main
-    print(main())
+    puzzle_num = sys.argv[1]
+    with open(f"inputs/{puzzle_num}.txt", "r") as f:
+        input_file = f.read()
+    print(main(input_file))
 
 if __name__ == "__main__":
     my_main()
--- tests/test_solutions.py
+++ tests/test_solutions.py
@@ -2,6 +2,9 @@ import pytest
 from solutions.puzzle1.solution import main
 
 def test_solution1():
-    pt1_ans, pt2_ans = main()
+    input_path = "tests/my_inputs/1.txt"
+    with open(input_path, "r") as f:
+        input_text = f.read()
+    pt1_ans, pt2_ans = main(input_text)
     assert pt1_ans == 1052
     assert pt2_ans == 6295
