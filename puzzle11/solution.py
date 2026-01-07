c8df5c76845631eae542d5c41773e2860718ede2
SansForSSBU <taylorjoseph3333@gmail.com>
2026-01-07 00:54:04 +0000

    WIP

--- puzzle11/solution.py
+++ puzzle11/solution.py
@@ -12,19 +12,28 @@ def get_upstream_nodes(graph, node):
     return [k for k,v in graph.items() if node in v]
 
 def find_n_routes(graph, start, end):
-    kv = {end: 1}
+    kv = {(end, 0): 1}
     routes = [(end, 0)]
     while len(routes) > 0:
         routes.sort(key=lambda x: x[1])
         node, depth = routes.pop(0)
         upstream_nodes = get_upstream_nodes(graph, node)
         for up in upstream_nodes:
-            if not up in kv.keys():
-                routes.append((up, depth+1))
-            kv[up] = kv.get(up,0) + kv[node]
+            entry = (up, depth+1)
+            if not entry in routes:
+                routes.append(entry)
+            kv[entry] = kv.get(entry,0) + kv[(node,depth)]
     
-    return kv.get(start, 0)
+    ans = 0
+    for k,v in kv.items():
+        if k[0] == start:
+            ans += v
+    return ans
 
+memo = {}
+def find_n_routes2(graph, start, end):
+
+    pass
 
 def solve_pt1(graph):
     return find_n_routes(graph, "you", "out")
@@ -32,6 +41,12 @@ def solve_pt1(graph):
 
 def solve_pt2(graph):
     # 11350111374360 too low
+    # 13620133649232 too low
+    # 247031466393430 total (no restrictions)
+    # 21677843348293140 wrong
+    # 393474305030400
+    #n0 = find_n_routes(graph, "svr", "out")
+
     n1 = find_n_routes(graph, "svr", "fft")
     n2 = find_n_routes(graph, "fft", "dac")
     n3 = find_n_routes(graph, "dac", "out")
@@ -40,7 +55,7 @@ def solve_pt2(graph):
     n5 = find_n_routes(graph, "dac", "fft")
     n6 = find_n_routes(graph, "fft", "out")
 
-    return n1*n2*n3 + n4*n5*n6
+    return n1*n2*n3+n4*n5*n6
 
 print("Part 1 ans:", solve_pt1(connections))
 print("Part 2 ans:", solve_pt2(connections))
