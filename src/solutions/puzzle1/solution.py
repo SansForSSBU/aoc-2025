96282a3bf0323721255dec7ccad1e1b2bba03014
SansForSSBU <taylorjoseph3333@gmail.com>
2026-01-07 18:29:16 +0000

    Refactor part 1 so that the safe's state is stored in a Safe object, not global variables

--- src/solutions/puzzle1/solution.py
+++ src/solutions/puzzle1/solution.py
@@ -1,32 +1,36 @@
-with open("inputs/1.txt", "r") as f:
-    lines = f.read()
+class Safe:
+    def __init__(self):
+        self.dial = 50
 
-moves = lines.split("\n")
-moves = [(move[0], int(move[1:])) for move in moves if len(move) > 0]
-pass
-
-dial=50
-pt1_ans = 0
-pt2_ans = 0
-
-def move_dial(move):
-    global dial
-    global pt1_ans
-    global pt2_ans
-    n_moves = move[1]
-    for _ in range(n_moves):
-        if move[0] == "L":
-            dial -= 1
-        elif move[0] == "R":
-            dial += 1
-        dial = dial % 100
-        if dial == 0:
-            pt2_ans += 1
-    if dial == 0:
-        pt1_ans += 1
+    def move_dial(self, move):
+        pt1_clicks = 0
+        pt2_clicks = 0
+        n_moves = move[1]
+        for _ in range(n_moves):
+            if move[0] == "L":
+                self.dial -= 1
+            elif move[0] == "R":
+                self.dial += 1
+            self.dial = self.dial % 100
+            if self.dial == 0:
+                pt2_clicks += 1
+        if self.dial == 0:
+            pt1_clicks += 1
+        
+        return pt1_clicks, pt2_clicks
 
 def main():
+    with open("inputs/1.txt", "r") as f:
+        lines = f.read()
+
+    moves = lines.split("\n")
+    moves = [(move[0], int(move[1:])) for move in moves if len(move) > 0]
+    pt1_ans = 0
+    pt2_ans = 0
+    safe = Safe()
     for move in moves:
-        move_dial(move)
+        clicks_pt1, clicks_pt2 = safe.move_dial(move)
+        pt1_ans += clicks_pt1
+        pt2_ans += clicks_pt2
 
     return pt1_ans, pt2_ans
