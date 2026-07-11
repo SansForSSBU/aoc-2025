0a5d40bb592a88e500dd6dde3a2de35147eeb96e
Joseph Taylor <taylorjoseph3333@gmail.com>
2026-07-11 15:32:05 +0100

    WIP

---
+++ src/solutions/puzzle1/solution.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/queue.h>
+#include <string.h>
+
+typedef enum {
+    UNKNOWN = -1,
+    LEFT,
+    RIGHT
+} Direction;
+
+typedef struct {
+    Direction dir;
+    int clicks;
+} Instruction;
+
+struct node {
+    Instruction data;
+    TAILQ_ENTRY(node) entries;
+};
+
+TAILQ_HEAD(QueueHead, node) head;
+
+Direction to_direction (char c)
+{
+    if (c == 'L')
+    {
+        return LEFT;
+    }
+    else if (c == 'R')
+    {
+        return RIGHT;
+    }
+    return UNKNOWN;
+}
+
+Instruction parse_instruction(char* line)
+{
+    Direction dir = to_direction(line[0]);
+    int clicks;
+    int items_read = sscanf(&line[1], "%d", &clicks);
+    Instruction ret = {dir, clicks};
+    return ret;
+}
+
+int main(int argc, char* argv[])
+{
+    FILE *file = fopen("/home/joseph/repos/aoc-2025/tests/my_inputs/1.txt", "r");
+    if (file == NULL) {
+        perror("Error opening file");
+        return 1;
+    }
+
+    TAILQ_INIT(&head);
+    char buffer[256];
+    while (fgets(buffer, sizeof(buffer), file) != NULL)
+    {
+        Instruction i = parse_instruction(buffer);
+        struct node *n1 = malloc(sizeof(struct node));
+        n1->data = i;
+        TAILQ_INSERT_TAIL(&head, n1, entries);
+    }
+    struct node *item;
+    TAILQ_FOREACH(item, &head, entries) {
+        printf("%d\n", item->data.clicks);
+    }
+    fclose(file);
+    return 0;
+}
