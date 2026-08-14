4f5d84677d29f334748a2ced9f52338aca15a4f6
Joseph Taylor <taylorjoseph3333@gmail.com>
2026-08-14 13:19:56 +0100

    Add integer conversions to input parsing

--- src/solutions/puzzle2/solution.c
+++ src/solutions/puzzle2/solution.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <malloc.h>
+#include <limits.h>
+#include <errno.h>
+#include <stdbool.h>
 
 typedef struct Range {
-    char* num1;
-    char* num2;
+    int num1;
+    int num2;
 } Range;
 
 typedef struct Node {
@@ -12,20 +16,45 @@ typedef struct Node {
     struct Node* next;
 } Node;
 
-Range* create_range(const char* n1, const char* n2) {
+Range* create_range(const int n1, const int n2) {
     Range* r = (Range*)malloc(sizeof(Range));
     if (r == NULL) return NULL;
 
-    r->num1 = strdup(n1);
-    r->num2 = strdup(n2);
+    r->num1 = n1;
+    r->num2 = n2;
 
     return r;
 }
 
 void free_range(Range* r) {
-    free(r->num1);
-    free(r->num2);
     free(r);
+}
+
+bool safe_str_to_int(const char* str, int* out)
+{
+    if (str == NULL || *str == '\0') {
+        return false;
+    }
+
+    char* endptr = NULL;
+    errno = 0;
+
+    long val = strtol(str, &endptr, 10);
+
+    if (errno == ERANGE) {
+        return false;
+    }
+
+    if (*endptr != '\0') {
+        return false;
+    }
+
+    if (val < INT_MIN || val > INT_MAX) {
+        return false;
+    }
+
+    *out = (int)val;
+    return true;
 }
 
 int main(int argc, char* argv[])
@@ -42,7 +71,14 @@ int main(int argc, char* argv[])
     while (token != NULL) {
         char* num1 = strtok_r(token, "-", &saveptr2);
         char* num2 = strtok_r(NULL, "-", &saveptr2);
-        Range* r = create_range(num1, num2);
+        int start;
+        int end;
+        Range* r;
+        if (safe_str_to_int(num1, &start) && safe_str_to_int(num2, &end))
+        {
+            r = create_range(start, end);
+            printf("%d-%d\n", r->num1, r->num2);
+        }
         token = strtok_r(NULL, ",", &saveptr1);
     }
     return 0;
